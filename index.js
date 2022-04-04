df2a70cb80747fd10b15f0e53fe495d469f8daa0
volkovily <volkov.ilyya@gmail.com>
2022-04-04 18:27:56 +0300

    Added 2 extra tiles

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function rangeSlide() {
-    let range = document.getElementById('myRange').value;
-    if (range == 5) {
-        console.log('it is 5')
-        range.className = range.className
-        .replace('hidden', '')
-        .trim()
+    const pink = document.getElementById('pink')
+    const yellow = document.getElementById('yellow')
+    const range = document.getElementById('myRange').value
+    
+    if (range >= 5 ) {
+        pink.classList.remove("hidden");
+    } else {
+        pink.classList.add("hidden");
+    }
+
+    if (range >= 6) {
+        yellow.classList.remove("hidden");
+    } else {
+        yellow.classList.add("hidden");
     }
 }
